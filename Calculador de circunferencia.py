abbd759a0ac7615abb3c50390c992001ad8095dc
Dieg0Alquinta14 <129990260+Dieg0Alquinta14@users.noreply.github.com>
2023-04-05 15:10:30 -0400

    Add files via upload

---
+++ Calculador de circunferencia.py
@@ -0,0 +1,8 @@
+import math
+def CalculoCircunferencia(radio):
+    pi = 3.141592
+    return 2 * pi * radio
+
+print ("La Circunferencia del radio 3: {:.6f}".format(CalculoCircunferencia(3)))
+print ("La Circunferencia del radio 8: {:.6f}".format(CalculoCircunferencia(8)))
+print ("La Circunferencia del radio 10: {:.6f}".format(CalculoCircunferencia(10)))
